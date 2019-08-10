2f710804ec1afb04e97789acd588ce8da0027128
ele94 <lena.ageitos@gmail.com>
2019-08-10 16:26:28 +0200

    model change for tests

--- dqnAgents.py
+++ dqnAgents.py
@@ -58,7 +58,7 @@ class PacmanDQNAgent(ReinforcementAgent):
         self.epsilon = 1.0
         self.alpha = 0.001
         #self.alpha = 1e-6
-        self.discount = 0.95
+        self.discount = 0.99
         self.batch_size = 35
         self.alpha_decay = 0.01
         self.frame_width = 85
@@ -67,11 +67,12 @@ class PacmanDQNAgent(ReinforcementAgent):
         self.new_episode = True
 
         self.memory = deque(maxlen=100000)
-        self.model = self._build_model()
+        #self.model = self._build_model()
+        self.model = self.atari_model()
 
         print 'INIT'
 
-        numpy.set_printoptions(threshold=sys.maxsize)
+        #numpy.set_printoptions(threshold=sys.maxsize)
 
 
 
@@ -138,13 +139,12 @@ class PacmanDQNAgent(ReinforcementAgent):
 
         if self.image is None: # we only need to compute it the first time. afterwards, well get the nextframe
             self.new_episode = True
-            print 'self.image was none'
             self.image = getFrame()
             self.image = np.array(self.image)
             self.image = resize(self.image, (self.frame_height, self.frame_width), order=0, anti_aliasing=True)
             #self.image = 255 * self.image
             #self.image = np.uint8(self.image)
-            self.image = np.reshape(self.image, [1,self.frame_height, self.frame_width,3])
+            #self.image = np.reshape(self.image, [1,self.frame_height, self.frame_width,3])
             self.image = 255 * self.image
             self.image = np.uint8(self.image)
         else:
@@ -161,8 +161,16 @@ class PacmanDQNAgent(ReinforcementAgent):
             #state_matrix = np.reshape(np.array(state_matrix), [1, self.state_size])
             #state_matrix = np.reshape(state_matrix, (1, self.frame_width, self.frame_height))
 
-            act_values = self.model.predict(self.image/255.0)
+            #act_values = self.model.predict(self.image/255.0)
+            self.testModel(self.image)
+            print "-----------------------------------------"
 
+            act_values = self.model.predict([np.reshape(self.image, [1,self.frame_height, self.frame_width,3])/255.0, np.ones((1,5))])
+            print act_values
+            print np.argmax(act_values[0])
+            print PACMAN_ACTIONS[(np.argmax(act_values[0]))]
+            print PACMAN_ACTIONS[np.argmax(act_values[0])]
+            print PACMAN_ACTIONS[np.argmax(act_values)]
             for value in act_values[0]:
                 action = PACMAN_ACTIONS[(np.argmax(act_values[0]))]
                 if action not in legalActions:
@@ -207,7 +215,7 @@ class PacmanDQNAgent(ReinforcementAgent):
             self.nextImage = getFrame()
             self.nextImage = numpy.array(self.nextImage)
             self.nextImage = resize(self.nextImage, (self.frame_width, self.frame_height))
-            self.nextImage = np.reshape(self.nextImage, [1, self.frame_height, self.frame_width, 3])
+            #self.nextImage = np.reshape(self.nextImage, [1, self.frame_height, self.frame_width, 3])
             self.nextImage = 255 * self.nextImage
             self.nextImage = np.uint8(self.nextImage)
         else:
@@ -229,7 +237,7 @@ class PacmanDQNAgent(ReinforcementAgent):
         #if self.episodesSoFar < self.numTraining:
 
 
-        if len(self.memory) > 5*self.batch_size:
+        if len(self.memory) > 1*self.batch_size:
             self.replay(self.batch_size)
 
         if self.count % 1000 == 0:
@@ -301,12 +309,20 @@ class PacmanDQNAgent(ReinforcementAgent):
 
     def remember(self, state, action, reward, next_state, done):
         action_index = PACMAN_ACTIONS.index(action)
-        self.memory.append((state, action_index, reward, next_state, done))
+        action_encoded = self.one_hot_encode(action_index)
+        reward = transform_reward(reward)
+        # if next_state is None:
+        #     next_state = state
+        print action, reward, done
+        self.memory.append((state, action_encoded, reward, next_state, done))
+
 
     def replay(self, batch_size):
         x_batch, y_batch = [], []
         minibatch = random.sample(self.memory, batch_size)
-        for state, action_index, reward, next_state, done in minibatch:
+        states, actions, rewards, next_states, dones = [], [], [], [], []
+        for state, action, reward, next_state, done in minibatch:
+
             # print 'state: ', state
             # print 'action_index: ', action_index
             # print 'reward: ', reward
@@ -316,17 +332,116 @@ class PacmanDQNAgent(ReinforcementAgent):
             # img.save("frames/my"+str(datetime.now())+".png")
             # img = Image.fromarray(next_state[0], 'RGB')
             # img.save("frames/my"+str(datetime.now())+".png")
-            state=state/255.0
-            next_state=next_state/255.0
-            y_target = self.model.predict(state)
-            y_target[0][action_index] = reward if done else reward + self.discount * np.max(self.model.predict(next_state)[0])
-            x_batch.append(state[0])
-            y_batch.append(y_target[0])
+            if next_state is None:
+                next_state = state
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+            next_states.append(next_state)
+            dones.append(done)
+            # state=state/255.0
+            # if next_state is not None:
+            #     next_state=next_state/255.0
+            # y_target = self.model.predict(state)
+            # y_target[0][action_index] = reward if done else reward + self.discount * np.max(self.model.predict(next_state)[0])
+            # x_batch.append(state[0])
+            # y_batch.append(y_target[0])
+            #
+            # next_Q_values = self.model.predict([state, np.ones(actions.shape)])
+            # next_Q_values[done] = 0
 
-        self.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
+        #self.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
         #W_Input_Hidden = self.model.layers[0].get_weights()[0]
         #print W_Input_Hidden
+        minibatch = np.array(minibatch)
+        # print type(minibatch[:,0])
+        # print minibatch[:,4]
+        #self.fit_batch(minibatch[:,0], minibatch[:,1], minibatch[:,2], minibatch[:,3], minibatch[:,4])
+        self.fit_batch(np.array(states),np.array(actions),np.array(rewards),np.array(next_states),np.array(dones))
+        #self.fit_batch(minibatch)
 
         if self.epsilon > self.epsilon_min:
             #None
             self.epsilon *= self.epsilon_decay
+
+
+    def fit_batch(self, start_states, actions, rewards, next_states, is_terminal):
+        """Do one deep Q learning iteration.
+    
+        Params:
+        - model: The DQN
+        - gamma: Discount factor (should be 0.99)
+        - start_states: numpy array of starting states
+        - actions: numpy array of one-hot encoded actions corresponding to the start states
+        - rewards: numpy array of rewards corresponding to the start states and actions
+        - next_states: numpy array of the resulting states corresponding to the start states and actions
+        - is_terminal: numpy boolean array of whether the resulting state is terminal
+    
+        """
+        # First, predict the Q values of the next states. Note how we are passing ones as the mask.
+        #print actions.shape
+        print actions.shape
+        next_Q_values = self.model.predict([next_states, np.ones(actions.shape)])
+        # The Q values of the terminal states is 0 by definition, so override them
+        next_Q_values[is_terminal] = 0
+        # The Q values of each start state is the reward + gamma * the max next state Q value
+        Q_values = rewards + self.discount * np.max(next_Q_values, axis=1)
+        #print Q_values
+        # Fit the keras model. Note how we are passing the actions as the mask and multiplying
+        # the targets by the actions.
+        self.model.fit(
+            [start_states, actions], actions * Q_values[:, None],
+            nb_epoch=1, batch_size=len(start_states), verbose=0
+        )
+
+
+    def one_hot_encode(self,action_index):
+        actions = np.zeros(5)
+        actions[action_index] = 1
+        return actions
+
+    def atari_model(self):
+        # We assume a theano backend here, so the "channels" are first.
+        n_actions=self.action_size
+        ATARI_SHAPE = (85, 85, 3)
+
+        # With the functional API we need to define the inputs.
+        frames_input = keras.layers.Input(ATARI_SHAPE, name='frames')
+        actions_input = keras.layers.Input((n_actions,), name='mask')
+
+        # Assuming that the input frames are still encoded from 0 to 255. Transforming to [0, 1].
+        normalized = keras.layers.Lambda(lambda x: x / 255.0)(frames_input)
+
+        conv_1 = keras.layers.convolutional.Convolution2D(
+            16, 8, 8, subsample=(4, 4), activation='relu'
+        )(normalized)
+        conv_2 = keras.layers.convolutional.Convolution2D(
+            32, 4, 4, subsample=(2, 2), activation='relu'
+        )(conv_1)
+        # Flattening the second convolutional layer.
+        conv_flattened = keras.layers.core.Flatten()(conv_2)
+        # "The final hidden layer is fully-connected and consists of 256 rectifier units."
+        hidden = keras.layers.Dense(256, activation='relu')(conv_flattened)
+        # "The output layer is a fully-connected linear layer with a single output for each valid action."
+        output = keras.layers.Dense(n_actions)(hidden)
+        # Finally, we multiply the output by the mask!
+        #filtered_output = keras.layers.merge([output, actions_input], mode='mul')
+        filtered_output = keras.layers.Multiply()([output, actions_input])
+
+        self.model = keras.models.Model(input=[frames_input, actions_input], output=filtered_output)
+        optimizer = keras.optimizers.RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
+        self.model.compile(optimizer='adam', loss='mse', metrics=["accuracy"])
+        return self.model
+
+    def testModel(self, image):
+        act_values = self.model.predict(
+            [np.reshape(image, [1, self.frame_height, self.frame_width, 3]) / 255.0, np.ones((1, 5))])
+        print act_values
+        self.replay(1)
+        act_values = self.model.predict(
+            [np.reshape(image, [1, self.frame_height, self.frame_width, 3]) / 255.0, np.ones((1, 5))])
+        print act_values
+
+def transform_reward(reward):
+    return np.sign(reward)
+
