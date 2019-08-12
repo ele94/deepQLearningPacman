53b3ab41f18be50f2cdeecfddd1e8868af570b80
ele94 <lena.ageitos@gmail.com>
2019-08-12 11:12:06 +0200

    learning dqn agent with double deep q-learning and changes in loss function

--- dqnAgents.py
+++ dqnAgents.py
@@ -13,6 +13,7 @@ from featureExtractors import *
 import random,util,math,keras
 from keras.optimizers import SGD
 from skimage.transform import resize
+from skimage import color
 import tensorflow as tf
 
 from datetime import datetime
@@ -50,64 +51,25 @@ class PacmanDQNAgent(ReinforcementAgent):
         "*** YOUR CODE HERE ***"
         self.action_size = 5
         self.epsilon_min = 0.1
-        self.epsilon_decay = 0.999
+        self.epsilon_decay = 0.99
         self.count = 0
         self.init = 0
         self.epsilon = 1.0
         self.alpha = 0.001
         self.discount = 0.99
         self.batch_size = 35
-        self.alpha_decay = 0.01
         self.frame_width = 85
         self.frame_height = 85
+        self.state_size = 3
         self.image = None
         self.new_episode = True
+        self.training = True
 
         self.memory = deque(maxlen=100000)
         self.model = self._build_model()
+        self.double_model = self._build_model()
 
-        print 'INIT'
-
-    # def getQValue(self, state, action):
-    #     """
-    #       Returns Q(state,action)
-    #       Should return 0.0 if we have never seen a state
-    #       or the Q node value otherwise
-    #     """
-    #     "*** YOUR CODE HERE ***"
-    #     #return self.QValueCounter[(state, action)]
-    #
-    #
-    # def computeValueFromQValues(self, state):
-    #     """
-    #       Returns max_action Q(state,action)
-    #       where the max is over legal actions.  Note that if
-    #       there are no legal actions, which is the case at the
-    #       terminal state, you should return a value of 0.0.
-    #     """
-    #     "*** YOUR CODE HERE ***"
-    #     if not self.getLegalActions(state): return 0
-    #
-    #     best_action = self.computeActionFromQValues(state)
-    #     return self.getQValue(state, best_action)
-    #
-    # def computeActionFromQValues(self, state):
-    #     """
-    #       Compute the best action to take in a state.  Note that if there
-    #       are no legal actions, which is the case at the terminal state,
-    #       you should return None.
-    #     """
-    #     "*** YOUR CODE HERE ***"
-    #     if not self.getLegalActions(state): return None
-    #
-    #     best_action = None
-    #     best_value = float('-inf')
-    #     for action in self.getLegalActions(state):
-    #         if self.getQValue(state, action) > best_value:
-    #             best_value = self.getQValue(state, action)
-    #             best_action = action
-    #     return best_action
-
+        #self.model.load_weights("models/model.h5") #uncomment to load old weights
 
     def getAction(self, state):
         """
@@ -122,43 +84,22 @@ class PacmanDQNAgent(ReinforcementAgent):
         """
         # Pick Action
         legalActions = self.getLegalActions(state)
-        # if 'Stop' in legalActions:
-        #     legalActions.remove('Stop')
-
         action = None
+
         "*** YOUR CODE HERE ***"
         if not self.getLegalActions(state): return action  # Terminal State, return None
 
-        if self.image is None: # we only need to compute it the first time. afterwards, well get the nextframe
-            self.new_episode = True
-            print 'self.image was none'
-            self.image = getFrame()
-            self.image = np.array(self.image)
-            self.image = resize(self.image, (self.frame_height, self.frame_width), order=0, anti_aliasing=True)
-            #self.image = 255 * self.image
-            #self.image = np.uint8(self.image)
-            self.image = np.reshape(self.image, [1,self.frame_height, self.frame_width,3])
-            self.image = 255 * self.image
-            self.image = np.uint8(self.image)
-        else:
+        #if self.image is None: # we only need to compute it the first time. afterwards, well get a copy from nextframe
+        if self.new_episode:
+            # self.new_episode = True
+            self.image = self.process_frame(getFrame())
             self.new_episode = False
-        #img = Image.fromarray(self.image[0], 'RGB')
-        #img.save("frames/my"+str(datetime.now())+".png")
 
-        #if self.count > 2:
-            #print "------------------------------"
-            #self.test(self.image)
-            #print "------------------------------"
-        #print 'Epsilon value: ', self.epsilon
+        # epsilon greedy: exploit - explore
         if self.epsilon > random.random():
             action = random.choice(legalActions)  # Explore
         else:
-            #action = self.computeActionFromQValues(state)  # Exploit
-            #state_matrix = self.getStateMatrices(state)
-            #state_matrix = np.reshape(np.array(state_matrix), [1, self.state_size])
-            #state_matrix = np.reshape(state_matrix, (1, self.frame_width, self.frame_height))
-
-            act_values = self.model.predict(self.image/255.0)
+            act_values = self.model.predict(self.image/255.0) # Exploit
 
             for value in act_values[0]:
                 action = PACMAN_ACTIONS[(np.argmax(act_values[0]))]
@@ -167,20 +108,9 @@ class PacmanDQNAgent(ReinforcementAgent):
                 else:
                     break
 
-            # action = PACMAN_ACTIONS[(np.argmax(act_values[0]))] # returns action
-            # print 'chosen action: ', action
-            #
-            # if action not in legalActions:
-            #     print 'action was not legal'
-            #
-            #     for value in act_values:
-            #         act_values[0, np.argmax(act_values[0])] = -1000
-            #         action = PACMAN_ACTIONS[(np.argmax(act_values[0]))]
-            #         if action in legalActions:
-            #             break
-            #     print 'chose action: ', action
-                #action = 'Stop'
-                #action = random.choice(legalActions)
+            if action not in legalActions:
+                print 'action was not legal'
+                action = 'Stop'
 
         self.doAction(state, action)
         return action
@@ -194,37 +124,34 @@ class PacmanDQNAgent(ReinforcementAgent):
           NOTE: You should never call this function,
           it will be called on your behalf
         """
+        "*** YOUR CODE HERE ***"
         if not self.getLegalActions(nextState):
             done = True
         else:
             done = False
-        "*** YOUR CODE HERE ***"
 
         if not done:
-            self.nextImage = getFrame()
-            self.nextImage = numpy.array(self.nextImage)
-            self.nextImage = resize(self.nextImage, (self.frame_width, self.frame_height))
-            self.nextImage = np.reshape(self.nextImage, [1, self.frame_height, self.frame_width, 3])
-            self.nextImage = 255 * self.nextImage
-            self.nextImage = np.uint8(self.nextImage)
+            self.nextImage = self.process_frame(getFrame())
         else:
             self.nextImage = None
 
-        self.count += 1
-        if not self.new_episode: #no guardamos la primera imagen porque da problemas
+        if not self.new_episode: # saving the first episode gives us trouble, so we skip it
             self.remember(self.image, action, reward, self.nextImage, done)
 
         # entrenamos la red neuronal solo mientras estamos en entrenamiento
         #if self.episodesSoFar < self.numTraining:
         #if self.episodesSoFar < self.numTraining:
 
-        if len(self.memory) > 5*self.batch_size:
+        self.count += 1
+        if len(self.memory) > 5*self.batch_size and self.training: # we only update the network while it's training
             self.replay(self.batch_size)
 
+        # we save the model every 1000 steps for safekeeping
         if self.count % 1000 == 0:
-            self.model.save_weights("models/model" + str(self.count) + ".h5")
-            print "saved file: models/model" + str(self.count) + ".h5"
+            self.model.save_weights("models/model.h5")
+            print "saved file: models/model.h5"
 
+        # we save the frames every 10000 steps for debugging purposes
         if self.count % 10000 == 0 and not self.new_episode:
             img = Image.fromarray(self.image[0], 'RGB')
             img.save("frames/"+str(datetime.now())+"image.png")
@@ -234,48 +161,25 @@ class PacmanDQNAgent(ReinforcementAgent):
 
         self.image = self.nextImage #updating old image
 
+    def final(self, state):
+        "Called at the end of each game."
+        # call the super-class final method
+        ReinforcementAgent.final(self, state)
 
-    # def getPolicy(self, state):
-    #     return self.computeActionFromQValues(state)
-    #
-    # def getValue(self, state):
-    #     return self.computeValueFromQValues(state)
-
-
-    # def final(self, state):
-    #     "Called at the end of each game."
-    #     # call the super-class final method
-    #     ReinforcementAgent.final(self, state)
-    #
-    #     state_matrix = self.getStateMatrices(state)
-    #     nextState_matrix = self.getStateMatrices(self.lastState)
-    #     state_matrix = np.reshape(state_matrix, [1, self.state_size])
-    #     nextState_matrix = np.reshape(nextState_matrix, [1, self.state_size])
-    #
-    #     self.remember(state_matrix, action, reward, nextState_matrix, done)
-    #
-    #     # did we finish training?
-    #     if self.episodesSoFar == self.numTraining:
-    #         # you might want to print your weights here for debugging
-    #         "*** YOUR CODE HERE ***"
-    #         pass
+        self.new_episode = True
+        # did we finish training?
+        if self.episodesSoFar == self.numTraining:
+            # you might want to print your weights here for debugging
+            "*** YOUR CODE HERE ***"
+            self.training = False
+            pass
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
-        # model = Sequential()
-        # model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-        # model.add(Dense(48, activation='relu'))
-        # model.add(Dense(self.action_size, activation='linear'))
-        # model.compile(loss='mse', optimizer=Adam(lr=self.alpha, decay=self.alpha_decay))
-
         model = Sequential()
-        #STATE_LENGTH = self.state_size
-        FRAME_WIDTH = self.frame_width
-        FRAME_HEIGHT = self.frame_height
-        STATE_LENGTH = 3
         model.add(Convolution2D(32, 8, 8, subsample=(4, 4), activation='relu',
         #                        input_shape=(STATE_LENGTH, FRAME_WIDTH, FRAME_HEIGHT)))
-                                 input_shape=(FRAME_HEIGHT, FRAME_WIDTH, STATE_LENGTH)))
+                                 input_shape=(self.frame_height, self.frame_width, self.state_size)))
         model.add(Convolution2D(64, 4, 4, subsample=(2, 2), activation='relu'))
         model.add(Convolution2D(64, 3, 3, subsample=(1, 1), activation='relu'))
         model.add(Flatten())
@@ -283,8 +187,6 @@ class PacmanDQNAgent(ReinforcementAgent):
         model.add(Dense(self.action_size))
         #model.compile(loss='mse', optimizer=Adam(lr=self.alpha))
         model.compile(loss='logcosh', optimizer='adam', metrics= ["accuracy"])
-        #model.compile(loss=losses.mean_squared_logarithmic_error, optimizer='adam', metrics= ["accuracy"])
-
         return model
 
     def remember(self, state, action, reward, next_state, done):
@@ -296,39 +198,41 @@ class PacmanDQNAgent(ReinforcementAgent):
         x_batch, y_batch = [], []
         minibatch = random.sample(self.memory, batch_size)
         for state, action_index, reward, next_state, done in minibatch:
-            # print 'state: ', state
-            # print 'action_index: ', action_index
-            # print 'reward: ', reward
-            # print 'next_state: ', next_state
-            # print 'done: ', done
-            # img = Image.fromarray(state[0], 'RGB')
-            # img.save("frames/my"+str(datetime.now())+".png")
-            # img = Image.fromarray(next_state[0], 'RGB')
-            # img.save("frames/my"+str(datetime.now())+".png")
+
             state=state/255.0
             if next_state is not None:
                 next_state=next_state/255.0
+
             y_target = self.model.predict(state)
-            #print "y_target: ", y_target
             y_target[0][action_index] = reward if done else reward + self.discount * np.max(self.model.predict(next_state)[0])
-            #print "y_target[0][action_index] updated: ", y_target
             x_batch.append(state[0])
             y_batch.append(y_target[0])
 
-        self.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=1)
-        #W_Input_Hidden = self.model.layers[0].get_weights()[0]
-        #print W_Input_Hidden
+        #self.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
+        self.double_model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
 
-        if self.epsilon > self.epsilon_min:
-            #None
+        # we update the double network weights to the main network every 100 steps
+        if self.count % 100 == 0:
+            self.double_model.save_weights("models/double_model.h5")
+            self.model.load_weights("models/double_model.h5")
+
+        if self.epsilon > self.epsilon_min: # TODO: add timer before starting decay?
             self.epsilon *= self.epsilon_decay
 
+    ##### HELPER METHODS #########
     def normalize_reward(self,reward):
-        if reward == -1:
-            reward = 0
         return np.sign(reward)
 
-    def test(self, image):
+    def process_frame(self, frame):
+        frame = np.array(frame)
+        frame = resize(frame, (self.frame_width, self.frame_height))
+        frame = np.reshape(frame, [1, self.frame_height, self.frame_width, self.state_size])
+        frame = 255 * frame
+        frame = np.uint8(frame)
+        return frame
+
+    # method for debugging
+    def test(self):
         print self.model.predict(self.image/255.0)
         self.replay(1)
         print self.model.predict(self.image/255.0)
